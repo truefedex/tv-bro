5bdc2083cd683f7ac34c4c775698f7ec00872f74
Fedir Tsapana <fedir.tsapana.contractor@fujifilm.com>
2022-04-30 14:22:04 +0300

    fix #94 - possibly fixes #94 (crash on switching to/from incognito mode)

--- app/src/main/java/com/phlox/tvwebbrowser/activity/main/MainActivity.kt
+++ app/src/main/java/com/phlox/tvwebbrowser/activity/main/MainActivity.kt
@@ -370,8 +370,14 @@ open class MainActivity : AppCompatActivity(), ActionBar.Callback {
     }
 
     override fun onDestroy() {
-        jsInterface.setActivity(null)
-        tabsModel.onDetachActivity()
+        //here properties can be uninitialized in case of wrong activity for incognito mode
+        //detection and force activity restart in onCreate()
+        if (::jsInterface.isInitialized) {
+            jsInterface.setActivity(null)
+        }
+        if (::tabsModel.isInitialized) {
+            tabsModel.onDetachActivity()
+        }
         super.onDestroy()
     }
 
